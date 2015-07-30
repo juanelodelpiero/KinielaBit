08589a6bed3943efc7c396c3d55ad7673b14e33a
juanelodelpiero <juane.ramirezc@gmail.com>
2015-07-30 15:15:02 -0500

    Comienzo de proyecto

---
+++ wear/src/main/java/main/kinielabit/com/kinielabit/MyStubBroadcastActivity.java
@@ -0,0 +1,21 @@
+package main.kinielabit.com.kinielabit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("main.kinielabit.com.kinielabit.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
