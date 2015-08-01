7369c556cbf47833f5e8fdd83c04b6e6c7558ef7
juanelodelpiero <juanenrique_ramirez@EDIAZ-DESAR-47.local>
2015-07-31 19:19:15 -0500

    Toolbar
    Se agrega toolbar widget

--- mobile/src/main/java/main/kinielabit/com/kinielabit/MainActivity.java
+++ mobile/src/main/java/main/kinielabit/com/kinielabit/MainActivity.java
@@ -2,6 +2,7 @@ package main.kinielabit.com.kinielabit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
